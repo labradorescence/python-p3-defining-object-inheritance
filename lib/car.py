81f541365c401a3abe38bd31430eebf49f4429cc
labradorescence <orangenewyorkcreativeagency@gmail.com>
2022-12-27 15:13:49 -0500

    _

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,11 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
     pass
+
+car_one = Car(2,4)
+print(car_one.go()) #VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!
+print(car_one.fill_up_tank())
+#filling up!
